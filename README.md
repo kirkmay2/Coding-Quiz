1ae80acf3a676914310207d79bfefe7976970e00
Kirk Jr <kaptaink3@gmail.com>
2021-02-20 19:11:29 -0500

    Trivia Quiz

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Coding-Quiz
+<!-- This quiz included questions ranging from sports, social media, COVID and gaming. They were seven easy short and simple questions. Good luck! -->
---
+++ script.js
@@ -0,0 +1,216 @@
+
+var quizBody = document.getElementById("quiz");
+var resultsEl = document.getElementById("result");
+var finalScoreEl = document.getElementById("finalScore");
+var gameoverDiv = document.getElementById("gameover");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var highscoreDiv = document.getElementById("high-scorePage");
+var highscoreInputName = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highscore-initials");
+var endGameBtns = document.getElementById("endGameBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscoreDisplayScore = document.getElementById("highscore-score");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+
+
+var quizQuestions = [{
+    question: "What is the most popular social media app today?",
+    choiceA: "Instagram",
+    choiceB: "Facebook",
+    choiceC: "Tik Tok",
+    choiceD: "Twitter",
+    correctAnswer: "c"},
+  {
+    question: "Which game is a Playstation exclusive?",
+    choiceA: "Spider-Man: Miles Morales",
+    choiceB: "Gears of War",
+    choiceC: "Titanfall",
+    choiceD: "Halo",
+    correctAnswer: "a"},
+   {
+    question: "What gaming company makes Fortnite?",
+    choiceA: "Activision",
+    choiceB: "2k",
+    choiceC: "Rockstar ",
+    choiceD: "Epic games",
+    correctAnswer: "d"},
+    {
+    question: "Who won the NBA Most Valuable Player Award in the 2010-2011 season?",
+    choiceA: "Dwight Howard",
+    choiceB: "Derrick Rose",
+    choiceC: "Kobe Bryant",
+    choiceD: "LeBron James",
+    correctAnswer: "b"},
+    {
+    question: "In what month did the US go into quarantine do to COVID-19?",
+    choiceA: "December",
+    choiceB: "April",
+    choiceC: "February",
+    choiceD: "March",
+    correctAnswer: "d"},  
+    {
+    question: "What year was the first virtual reality headset created?",
+    choiceA: "1995",
+    choiceB: "2011",
+    choiceC: "2020",
+    choiceD: "2001",
+    correctAnswer: "a"},
+    {
+    question: "Who won the Fifa World Cup in 2018?",
+    choiceA: "Croatia",
+    choiceB: "Jamaica",
+    choiceC: "France",
+    choiceD: "Brazil",
+    correctAnswer: "c"},
+
+
+    ];
+
+var finalQuestionIndex = quizQuestions.length;
+var currentQuestionIndex = 0;
+var timeLeft = 76;
+var timerInterval;
+var score = 0;
+var correct;
+
+
+function generateQuizQuestion(){
+    gameoverDiv.style.display = "none";
+    if (currentQuestionIndex === finalQuestionIndex){
+        return showScore();
+    } 
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    buttonA.innerHTML = currentQuestion.choiceA;
+    buttonB.innerHTML = currentQuestion.choiceB;
+    buttonC.innerHTML = currentQuestion.choiceC;
+    buttonD.innerHTML = currentQuestion.choiceD;
+};
+
+
+function startQuiz(){
+    gameoverDiv.style.display = "none";
+    startQuizDiv.style.display = "none";
+    generateQuizQuestion();
+
+    
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        quizTimer.textContent = "Time left: " + timeLeft;
+
+        if(timeLeft === 0) {
+          clearInterval(timerInterval);
+          showScore();
+        }
+      }, 1000);
+    quizBody.style.display = "block";
+}
+
+function showScore(){
+    quizBody.style.display = "none"
+    gameoverDiv.style.display = "flex";
+    clearInterval(timerInterval);
+    highscoreInputName.value = "";
+    finalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
+}
+
+
+submitScoreBtn.addEventListener("click", function highscore(){
+
+
+    if(highscoreInputName.value === "") {
+        alert("Initials cannot be blank");
+        return false;
+    }else{
+        var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+        var currentUser = highscoreInputName.value.trim();
+        var currentHighscore = {
+            name : currentUser,
+            score : score
+        };
+
+        gameoverDiv.style.display = "none";
+        highscoreContainer.style.display = "flex";
+        highscoreDiv.style.display = "block";
+        endGameBtns.style.display = "flex";
+
+        savedHighscores.push(currentHighscore);
+        localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+        generateHighscores();
+
+    }
+
+});
+
+
+function generateHighscores(){
+    highscoreDisplayName.innerHTML = "";
+    highscoreDisplayScore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+    for (i=0; i<highscores.length; i++){
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        highscoreDisplayName.appendChild(newNameSpan);
+        highscoreDisplayScore.appendChild(newScoreSpan);
+    }
+}
+
+
+function showHighscore(){
+    startQuizDiv.style.display = "none"
+    gameoverDiv.style.display = "none";
+    highscoreContainer.style.display = "flex";
+    highscoreDiv.style.display = "block";
+    endGameBtns.style.display = "flex";
+
+    generateHighscores();
+}
+
+
+function clearScore(){
+    window.localStorage.clear();
+    highscoreDisplayName.textContent = "";
+    highscoreDisplayScore.textContent = "";
+}
+
+
+function replayQuiz(){
+    highscoreContainer.style.display = "none";
+    gameoverDiv.style.display = "none";
+    startQuizDiv.style.display = "flex";
+    timeLeft = 76;
+    score = 0;
+    currentQuestionIndex = 0;
+}
+
+
+function checkAnswer(answer){
+    correct = quizQuestions[currentQuestionIndex].correctAnswer;
+
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+        score++;
+        alert("That Is Correct!");
+        currentQuestionIndex++;
+        generateQuizQuestion();
+       
+    }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+        alert("That Is Incorrect.")
+        currentQuestionIndex++;
+        generateQuizQuestion();
+       
+    }else{
+        showScore();
+    }
+}
+
+
+startQuizButton.addEventListener("click",startQuiz);
